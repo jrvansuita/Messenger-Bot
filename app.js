e1bb9b5112b52f12816960eb693a7462364910a6
jrvansuita <vansuita.jr@gmail.com>
2018-10-23 16:01:30 -0300

    gogogo

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: '15mb',
+  parameterLimit: 99999999
+}));
+
+
+app.post("/", (req, res) => {
+  res.sendStatus(200);
+  
+  handleIncoming(req.body, res);
+});
---
+++ app/handler/incoming.js
@@ -0,0 +1,38 @@
+module.exports = class Incoming{
+  handle(data){
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+      // Iterate over each entry
+      // There may be multiple if batched
+      data.entry.forEach(function(pageEntry) {
+        var pageID = pageEntry.id;
+        var timeOfEvent = pageEntry.time;
+
+        // Iterate over each messaging event
+        pageEntry.messaging.forEach(function(messagingEvent) {
+          if (messagingEvent.optin) {
+            //receivedAuthentication(messagingEvent);
+          } else if (messagingEvent.message) {
+            receivedMessage(messagingEvent.message);
+          } else if (messagingEvent.delivery) {
+            //receivedDeliveryConfirmation(messagingEvent);
+          } else if (messagingEvent.postback) {
+            //receivedPostback(messagingEvent);
+          } else if (messagingEvent.read) {
+            //receivedMessageRead(messagingEvent);
+          } else if (messagingEvent.account_linking) {
+            //receivedAccountLink(messagingEvent);
+          } else {
+            console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+          }
+        });
+      });
+    }
+  }
+};
+
+
+function receivedMessage(msg){
+  var text = msg.message.text;
+  console.log(text);
+}
